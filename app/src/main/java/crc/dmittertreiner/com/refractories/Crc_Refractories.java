15793745be2301cf69736c768ac4ce28ef29c338
donaldmittertreiner <32747535+donaldmittertreiner@users.noreply.github.com>
2017-10-12 20:29:46 +0200

    Initial commit

---
+++ app/src/main/java/crc/dmittertreiner/com/refractories/Crc_Refractories.java
@@ -0,0 +1,13 @@
+package crc.dmittertreiner.com.refractories;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Crc_Refractories extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crc__refractories);
+    }
+}
